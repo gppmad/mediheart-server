76b91a95b3e11e6e0412602ae1f0ba7edcd64fa1
Madkerberos <giuseppe.maddiona@gmail.com>
2020-08-28 22:43:25 +0200

    adding rest_token_auth to setting and did a first test

--- app_server/api/views/personalInfo.py
+++ app_server/api/views/personalInfo.py
@@ -1,12 +1,13 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated  
 from api.models.patient import Patient as Patient
 from api.models.bloodtype import BloodType as BloodType
 from api.serializers.serializers import PatientSerializer as PatientSerializer
 from api.serializers.serializers import PatientDelSerializer as PatientDelSerializer
-from api.serializers.serializers import PatientSerializer as DBModelSerializer #LEGACY
-from api.serializers.serializers import PatientDelSerializer as DBModelDelSerializer #LEGACY
-from api.models.patient import Patient as DBModel #LEGACY
+# from api.serializers.serializers import PatientSerializer as DBModelSerializer #LEGACY
+# from api.serializers.serializers import PatientDelSerializer as DBModelDelSerializer #LEGACY
+# from api.models.patient import Patient as DBModel #LEGACY
 
 from datetime import date
 from dateutil.relativedelta import relativedelta
@@ -15,9 +16,8 @@ import json
 import os
 
 class PersonalInfoView(APIView):
-    
-    """GET OBJECT(S) WITH ID PARAMETER"""
-    
+    permission_classes = (IsAuthenticated,)   
+
     #GET OBJECT WITH ID
     def get(self, request, id=""):
         api_response = {}
