bc51a1e80b1c40d0c85ee4aa67561264294b6873
Madkerberos <giuseppe.maddiona@gmail.com>
2020-08-29 22:34:24 +0200

    adding login token generator

--- app_server/api/urls.py
+++ app_server/api/urls.py
@@ -1,12 +1,14 @@
 from django.urls import path
 from . import views
 from api.views.personalInfo import PersonalInfoView,PersonalInfoModifyView
+from rest_framework.authtoken.views import obtain_auth_token
 
 urlpatterns = [
     #path('', views.index, name='index'),
     #path('personalInformations/<int:id>/', PersonalInformationsView.as_view()),
+    #path('personalInformations/modify/<int:id>/', PersonalInformationsModifyView.as_view())
+    path('token/', obtain_auth_token, name='api_token_auth'),  # <-- And here
     path('personalInfo/<int:id>/', PersonalInfoView.as_view()),
     path('personalInfo/', PersonalInfoView.as_view()),
-    #path('personalInformations/modify/<int:id>/', PersonalInformationsModifyView.as_view())
     path('personalInfo/modify/', PersonalInfoModifyView.as_view())
 ]
