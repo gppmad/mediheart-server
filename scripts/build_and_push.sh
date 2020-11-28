b1e9188f72286f3bfa77eea9afb1e2c2f6b0b10d
madkerberos <giuseppe.maddiona@gmail.com>
2020-11-28 20:07:06 +0100

    added scripts folder

---
+++ scripts/build_and_push.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+v=0.0.4
+
+docker build -f ./docker/qa/Dockerfile -t mediheart_qa_server:$v . --no-cache
+docker tag mediheart_qa_server:$v giuseppemaddiona/mh-server-qa:$v
+docker push giuseppemaddiona/mh-server-qa:$v 
