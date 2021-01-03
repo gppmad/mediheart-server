8b20f0b29c90dc2a5e206b1a325c411d336e1d13
madkerberos <giuseppe.maddiona@gmail.com>
2021-01-03 13:34:01 +0100

    changed folders parent

---
+++ api/models/__init__.py
@@ -0,0 +1,2 @@
+# from .patients import Patients as _Patients
+# from .bloodType import BloodType as _BloodType
--- app_server/api/models/__init__.py
+++
@@ -1,2 +0,0 @@
-from .patients import Patients as _Patients
-from .bloodType import BloodType as _BloodType
