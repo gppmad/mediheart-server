7f46b2544cc49cdc43bddb0578bfb03519068c58
madkerberos <giuseppe.maddiona@gmail.com>
2021-01-16 22:10:19 +0100

    updated docker config

--- build_and_push.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-v=0.0.5
-
-docker build -f ./docker/qa/Dockerfile -t mediheart_qa_server:$v . --no-cache
-docker tag mediheart_qa_server:$v giuseppemaddiona/mh-server-qa:$v
-docker push giuseppemaddiona/mh-server-qa:$v 
