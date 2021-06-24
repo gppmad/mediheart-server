67ec34f17db9407634212520a627a8ef2ee5268d
giuseppe <giuseppe.maddiona@gmail.com>
2021-06-24 22:02:03 +0200

    using only DRF

--- api/views/patients_management/patients_list.py
+++ api/views/patients_management/patients_list.py
@@ -1,5 +1,4 @@
 from django.db.models import F
-from django.http import JsonResponse
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from api.models.patients import Patients
@@ -7,13 +6,11 @@ from api.serializers.serializers import PatientsSerializer as PatientsSerializer
 
 
 class PatientsList (APIView):
-    
+
     # Get the list of all patients.
     def get(self, request):
-        
-        # patients_table = Patient.objects.all()
-        # patient_serializer = PatientSerializer(patients_table, many=True)
-        # return Response(patient_serializer.data)
 
-        patients_table = list(Patients.objects.all().values("firstname","lastname","birthDate","gender", patientId=F("id"), userId=F("fk_user")  ) )
-        return JsonResponse({'data':patients_table})
+        patients_table = list(Patients.objects
+                              .all()
+                              .values("firstname", "lastname", "birthDate", "gender", patientId=F("id"), userId=F("fk_user")))
+        return Response({'data': patients_table})
